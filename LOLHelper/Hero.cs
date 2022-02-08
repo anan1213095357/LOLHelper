990697092a631fdef2ec8b82c9b677485011ffcd
A <A@DESKTOP-UR2J4MK>
2022-02-08 12:54:25 +0800

    添加项目文件。

---
+++ LOLHelper/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOLHelper
+{
+    public enum Position
+    { 
+        上单,
+        打野,
+        中单,
+        射手,
+        辅助
+    }
+
+    public class Hero
+    {
+        public string Name { get; set; }
+        public Talent Talent { get; set; }
+        public int Order { get; set; }
+        public Position Position { get; set; }
+        public bool Available { get; set; } = true;
+    }
+}
---
+++ LOLHelper/Program.cs
@@ -0,0 +1,25 @@
+﻿using Stateless;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static LOLHelper.LOL_Client_Operation;
+
+namespace LOLHelper
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
